56a658d31f433a3186af60b4c616ac399572d3d4
ubervelocity <mihaitu.popescu@gmail.com>
2019-05-13 15:40:35 +0200

    boxplots and t-tests

--- eeganalproject.m
+++ eeganalproject.m
@@ -25,20 +25,64 @@ ft_multiplotTFR(cfg, freqdesc);
 
 % Store the powscptrm for easy access.
 pows = freqdesc.powspctrm;
+
+% Stores every value of FP1 electrode.
 fp1 = pows(:,1,:,:);
+
 
 % Produces a vector filled with the frequencies (,,X,) and times (,,,X).
 % This is what you are interested in observing.
 peaceTime = 91;
 angerTime = 158;
+
+% Reduces 1-sized dimension to convert matrix from 4D to 2D.
 squeezedAnger = squeeze(pows(angerTime,1,:,:));
-averageSqueezedAnger = mean(squeezedAnger);
 squeezedPeace = squeeze(pows(peaceTime,1,:,:));
+
+% Compute average of the 
+averageSqueezedAnger = mean(squeezedAnger);
 averageSqueezedPeace = mean(squeezedPeace);
+
+cnt = sum(averageSqueezedAnger > averageSqueezedPeace);
+
+% Compute mean of intersected times of the conditions excluding NaN values
+% for statistics.
+ang = nanmean(averageSqueezedAnger);
+peac = nanmean(averageSqueezedPeace);
+
+% Plot the channel, displaying the two conditions.
 figure(10);
 plot(squeezedAnger);
 figure(11);
 plot(squeezedPeace);
+
+% Use other variables to store the same information in squeezeAnger and
+% squeezePeace.
+angfp1 = squeeze(pows(angerTime, 1, :, :));
+peacfp1 = squeeze(pows(peaceTime, 1, :, :));
+
+% Take only the first frequency of the conditions. (9Hz in our case).
+angfp1_1 = squeeze(angfp1(1,:))';
+peacfp1_1 = squeeze(peacfp1(1,:))';
+
+% Group the variables for comparison using boxplots.
+group = [ones(size(angfp1_1)); 2 * ones(size(peacfp1_1))];
+
+% Plot boxplot for comparison.
+figure
+boxplot([angfp1_1; peacfp1_1],group);
+set(gca,'XTickLabel',{'angfp1','peacfp1'});
+
+% Perform t-test for the two conditions.
+% h = [1, 0] = [rejects h, ~rejects h].
+% p = p-value (significance :=  p <= 0.05).
+[h,p] = ttest2(angfp1_1, peacfp1_1);
+sprintf("h = %d", h)
+if p <= 0.05
+    sprintf("p = %.3f * ", p)
+else
+    sprintf("p = %.3f", p)
+end
 
 % Normalize data - FILLS WITH NaN!
 cpydata = TFRiccleanedB;
