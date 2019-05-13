4c20ae50c8b8103f63b45759fce720b4fcad93f0
ubervelocity <mihaitu.popescu@gmail.com>
2019-05-13 15:42:46 +0200

    added photos to repo

---
+++ teacher.m
@@ -0,0 +1,17 @@
+cfg = [];
+cfg.method = 'mtmconvol';
+cfg.taper = 'dpss';
+cfg.tapsmofrq = 2;
+cfg.output = 'pow';
+cfg.keeptrials = 'yes';
+cfg.foi = [4:50];
+cfg.t_ftimwin = ones(length(cfg.foi),1).*0.5;
+cfg.toi = -0.5:0.05:1;
+TFRhigh = ft_freqanalysis(cfg,hpdat);
+
+cfg = [];
+cfg.baseline = [];
+cfg.baselinetype = 'absolute';
+cfg.showlabels = 'yes';
+cfg.showoutline = 'yes';
+ft_multiplotTFR(cfg, TFRhigh);
