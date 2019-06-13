6079913206e4d59fb063ada891a3b618a0ee7ed4
ubervelocity <mihaitu.popescu@gmail.com>
2019-06-13 15:32:12 +0200

    succesffully converted matlab final_data to r.data

---
+++ lme.r
@@ -0,0 +1,8 @@
+library(R.matlab);
+library(lme4);
+library(languageR);
+setwd("C:/Users/redth/Documents/University/Bachelor/git/bachelor-project/data_files")
+
+# data$alphaResults[1,] returns all values on row 1.
+# data$alphaResults[,1] returns al values on column 1.
+
