d616f080138fbd97b2c2e42fdc6fc3cbbeb3bf91
ubervelocity <mihaitu.popescu@gmail.com>
2019-06-17 01:54:59 +0200

    progress on LME - init results

--- lme.r
+++ lme.r
@@ -1,8 +1,30 @@
 library(R.matlab);
 library(lme4);
+library(nlme);
 library(languageR);
 library(dplyr);
+require(MASS);
+library(lmerTest);
 setwd("C:/Users/redth/Documents/University/Bachelor/git/bachelor-project/data_files")
+
+findOutlier <- function(data, cutoff = 3) {
+  ## Calculate the sd
+  sds <- apply(data, 2, sd, na.rm = TRUE)
+  ## Identify the cells with value greater than cutoff * sd (column wise)
+  result <- mapply(function(d, s) {
+    which(d > cutoff * s)
+  }, data, sds)
+  result
+}
+
+removeOutlier <- function(data, outliers) {
+  result <- mapply(function(d, o) {
+    res <- d
+    res[o] <- NA
+    return(res)
+  }, data, outliers)
+  return(as.data.frame(result))
+}
 
 # data$alphaResults[1,] returns all values on row 1.
 # data$alphaResults[,1] returns al values on column 1.
@@ -46,3 +68,13 @@ new2 <- cbind(new, beta_frame);
 final_data <- cbind(new2, theta_frame);
 
 saveRDS(final_data, file = "final_data.rds");
+
+boxplot(`alpha 13` ~ isExp, data = final_data, outline = FALSE);
+
+final_data.model = lmer(`alpha 13` ~ isExp + (1|id), data = final_data);
+anova(final_data.model);
+
+qqnorm(final_data$`alpha 1`, pch = 1, frame = FALSE)
+qqline(final_data$`alpha 1`, col = "steelblue", lwd = 2)
+
+
